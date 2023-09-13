8ec8d53d21f1423a2ab0a64a8e71dd6f0a2aad94
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-09-13 19:36:20 -0400

    tasklist lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addEventListener("submit", (event) => {
+event.preventDefault();
+const list = document.querySelector("#tasks");
+const item = document.createElement("li");
+const input = event.target["new-task-description"].value;
+item.textContent = input ;
+list.appendChild(item);
+  })
 });
+const deleteButton = createElement('button');
+deleteButton.textContent = "X";
+newTask.appendChild(deleteButton);
+deleteButton.addEventListener("click" , () => {
+newTask.remove()
+});
+item.append(newTask);
+
+
